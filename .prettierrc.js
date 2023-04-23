3151f3bfcb015be40498908f10baf9515706715d
rui-rui-an <1405613785@qq.com>
2023-04-24 00:14:47 +0800

    “”基本完成

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,11 +1,11 @@
 module.exports = {
-  tabWidth: 4, // 指定每个缩进级别的空格数<int>，默认2
+  tabWidth: 2, // 指定每个缩进级别的空格数<int>，默认2
   useTabs: false, // 用制表符而不是空格缩进行<bool>，默认false
   printWidth: 300, // 一行的字符数，如果超过会进行换行，默认为80
   singleQuote: true, // 字符串是否使用单引号，默认为false，使用双引号
-  endOfLine: "auto", // 避免报错delete (cr)的错
-  proseWrap: "always",
+  endOfLine: 'auto', // 避免报错delete (cr)的错
+  proseWrap: 'always',
   semi: true, // 不加分号
-  trailingComma: "none", // 结尾处不加逗号
-  htmlWhitespaceSensitivity: "ignore", // 忽略'>'下落问题
+  trailingComma: 'none', // 结尾处不加逗号
+  htmlWhitespaceSensitivity: 'ignore' // 忽略'>'下落问题
 };
--- src/main.js
+++ src/main.js
@@ -1,17 +1,17 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-import ElementUI from 'element-ui'
-import 'element-ui/lib/theme-chalk/index.css'
-import 'normalize.css/normalize.css'
-import './assets/css/reset.css'
-import './router/router-config' // 路由守卫，做动态路由的地方
-Vue.use(ElementUI)
-Vue.config.productionTip = false
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+import 'normalize.css/normalize.css';
+import './assets/css/reset.css';
+import './router/router-config'; // 路由守卫，做动态路由的地方
+Vue.use(ElementUI);
+Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
-  render: h => h(App)
-}).$mount('#app')
+  render: (h) => h(App)
+}).$mount('#app');
