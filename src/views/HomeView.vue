25706a72d95d42842e51634dde43e5ba48e605ec
liaorui <liaor@suntang.com>
2023-04-23 18:02:49 +0800

    提交部分

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
+    <el-button type="primary">
+      element的按钮
+    </el-button>
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,30 @@
+const path = require('path')
+function resolve (dir) {
+  // 配置别名
+  return path.join(__dirname, dir)
+}
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  lintOnSave: false
+  lintOnSave: false,
+  configureWebpack: {
+    resolve: {
+      alias: {
+        '@': resolve('src')
+      }
+    }
+  },
+  // 样例
+  devServer: {
+    // 开启代理服务器 (方式1)   配置多个代理
+    proxy: {
+      '/api': {
+        // '/api'是自行设置的请求前缀
+        target: 'http://localhost:5000',
+        // ws: true,//用于支持websocket
+        changeOrigin: true, // 用于控制请求头中的host值
+        pathRewrite: { '^/api': '' } // 路径重写，（正则）匹配以api开头的路径为空（将请求前缀删除）
+      }
+    }
+  }
 })
